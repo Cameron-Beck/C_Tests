cb04ad59c230c7623e8ae1d56e3b2832b1b0a575
Cameron Beck <cameron.beckit@gmail.com>
2023-01-02 22:23:51 +0000

    added grade.c

---
+++ grade.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char grade = 'A';
+
+    switch(grade){
+    case 'A' :
+        printf("great");
+        break;
+    case 'B' :
+        printf("great");
+        break;
+    case 'C' :
+        printf("great");
+        break;
+    case 'D' :
+        printf("great");
+        break;
+    case 'F' :
+        printf("great");
+        break;
+    defualt :
+        printf("invalid");
+    }
+    return 0;
+}
